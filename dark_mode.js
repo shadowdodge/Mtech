5aed4cff1f02ae98950d0fd35d3189b294cf5c88
shadowdodge <landonheinrichs12@gmail.com>
2024-06-22 09:36:23 -0600

    dark mode assignment

---
+++ dark_mode.js
@@ -0,0 +1,86 @@
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+];
+const productContainer = $('#productContainer');
+
+$(document).ready(() => {
+  products.forEach((product) => {
+    if (product.inStock) {
+      const productItem = `
+        <div class="product">
+          <h3>${product.name}</h3>
+          <p>$${product.price.toFixed(2)}</p>
+        </div>
+      `;
+      productContainer.append(productItem);
+    }
+  });
+});
+document.getElementById('clickMe').addEventListener('click', () => {
+    document.getElementById('contentContainer').classList.toggle('darkMode');
+  });
